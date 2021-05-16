bf9e3b477ced09d91e06fadddb22a1aa04c19b9c
sek <noreoile@csu.fullerton.edu>
2021-05-16 13:48:50 -0700

    update

---
+++ app/src/main/java/com/example/cpsc411_project/ClickListener.kt
@@ -0,0 +1,42 @@
+package com.example.cpsc411_project
+
+import android.content.Context
+import androidx.core.view.GestureDetectorCompat
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ClickListener(context: Context, recyclerView: RecyclerView, private val listener: OnRecyclerClickListener)
+    : RecyclerView.SimpleOnItemTouchListener() {
+
+    private val TAG = "ClickListen"
+
+    interface OnRecyclerClickListener {
+        fun onItemClick(view: View, position: Int)
+        fun onItemLongClick(view: View, position: Int)
+    }
+
+    // add the gestureDetector
+    private val gestureDetector = GestureDetectorCompat(context, object : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            Log.d(TAG, ".onLongPress: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            Log.d(TAG, ".onLongPress calling listener.onItemLongClick")
+            if (childView != null) {
+                listener.onItemLongClick(childView, recyclerView.getChildAdapterPosition(childView))
+            }
+            return true
+        }
+    })
+
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+        Log.d(TAG, ".onInterceptTouchEvent: starts $e")
+        val result = gestureDetector.onTouchEvent(e)
+        Log.d(TAG, ".onInterceptTouchEvent() returning: $result")
+//        return super.onInterceptTouchEvent(rv, e)
+        return result
+    }
+}
